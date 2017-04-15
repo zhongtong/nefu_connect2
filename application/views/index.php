8d5772c1c85b75208edd517cfb76c805af1761de
zhongtong <397580500@qq.com>
2017-04-15 14:37:18 +0800

    东林匿名信息平台

---
+++ application/views/index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="<?php echo site_url();?>">
+    <link rel="shortcut icon" href="assets/font/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>campusInfo</title>
+</head>
+<body>
+    <div id="index">
+                <div class="title navbar navbar-fixed-top">
+                    <div class="title-left">
+                        <img src="assets/font/favicon.ico" alt="">
+                    </div>
+                    <div class="title-center">
+                        campus
+                    </div>
+                    <div class="title-right">
+                        <img src="assets/font/add.ico" alt="">
+                    </div>
+                 </div>
+        <div class="content">
+            <ul>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/woman1.jpg" alt="">
+                                <span>某同学·女</span>
+                            </div>
+                            <div class="content-header-right">3分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">这是东林匿名信息平台，哈哈！</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>3</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>0</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man1.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">12分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">想帮同学问一下 有一个互联网研究的访谈项目，需要绵阳的城市负责人，找地方，访谈的时候负责现场事情 两天4.24-4.25 路费住宿费报销 报酬一天二百左右 需要传简历过去 有没有人想去的 她说可以学到很多市场调研的东西</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>12</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>6</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man2.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">37分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">专科生，没什么优点，就是心态好点。94年的，已经出柜了，虽然家人态度不好但也不坏。感觉主要还是担心我以后的生活，也希望未来我能改变，所以责任感很强烈，性格也还好，长的一般。在这发贴，目的很简单，碰碰运气，想找个合得来对象，能出柜的那种。想着跟你一起努力，去共同面对种种困难，彼此爱且包容，因为想爱一辈子，慎聊。兄弟，我觉得你心态不错，加油！</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>8</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>2</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man1.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">12分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">想帮同学问一下 有一个互联网研究的访谈项目，需要绵阳的城市负责人，找地方，访谈的时候负责现场事情 两天4.24-4.25 路费住宿费报销 报酬一天二百左右 需要传简历过去 有没有人想去的 她说可以学到很多市场调研的东西</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>12</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>6</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man2.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">37分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">专科生，没什么优点，就是心态好点。94年的，已经出柜了，虽然家人态度不好但也不坏。感觉主要还是担心我以后的生活，也希望未来我能改变，所以责任感很强烈，性格也还好，长的一般。在这发贴，目的很简单，碰碰运气，想找个合得来对象，能出柜的那种。想着跟你一起努力，去共同面对种种困难，彼此爱且包容，因为想爱一辈子，慎聊。兄弟，我觉得你心态不错，加油！</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>8</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>2</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man1.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">12分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">想帮同学问一下 有一个互联网研究的访谈项目，需要绵阳的城市负责人，找地方，访谈的时候负责现场事情 两天4.24-4.25 路费住宿费报销 报酬一天二百左右 需要传简历过去 有没有人想去的 她说可以学到很多市场调研的东西</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>12</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>6</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper">
+                        <div class="content-header">
+                            <div class="content-header-left">
+                                <img src="assets/image/man2.jpg" alt="">
+                                <span>某同学·男</span>
+                            </div>
+                            <div class="content-header-right">37分钟前</div>
+                        </div>
+                        <div class="content-middle">
+                            <div class="middle-text">哈哈，专科生，没什么优点，就是心态好点。94年的，已经出柜了，虽然家人态度不好但也不坏。感觉主要还是担心我以后的生活，也希望未来我能改变，所以责任感很强烈，性格也还好，长的一般。在这发贴，目的很简单，碰碰运气，想找个合得来对象，能出柜的那种。想着跟你一起努力，去共同面对种种困难，彼此爱且包容，因为想爱一辈子，慎聊。兄弟，我觉得你心态不错，加油！</div>
+                        </div>
+                        <div class="content-footer">
+                            <div class="content-footer-love">
+                                <a href="javascript:;"><img src="assets/font/love.ico" alt=""></a>
+                                <span>8</span>
+                            </div>
+                            <div class="content-footer-comment">
+                                <a href="javascript:;"><img src="assets/font/comment.ico" alt=""></a>
+                                <span>2</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+
+            </ul>
+        </div>
+        <div class="footer">
+            <div class="footer-left">
+                <img src="assets/font/page-2.ico" alt="">
+            </div>
+            <div class="footer-right">
+                <img src="assets/font/person-1.ico" alt="">
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery-2.1.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/index.js"></script>
+</body>
+</html>
